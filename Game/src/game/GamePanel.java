d10cfc8c802e54ebc7fc1f4595bb0692a00890ad
Albi-back <94167011+Albi-back@users.noreply.github.com>
2024-02-19 00:26:10 +0000

    background

--- Game/src/game/GamePanel.java
+++ Game/src/game/GamePanel.java
@@ -9,6 +9,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import entity.Player;
+import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable{
     
@@ -17,22 +18,21 @@ public class GamePanel extends JPanel implements Runnable{
     final int scale = 3;
 
     public final int tileSize = originalTileSize * scale; //48x48 tile
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
     final int screenWidth = tileSize*maxScreenCol; //768 pixels
     final int screenHeight = tileSize*maxScreenRow; //576 pixels
 
     //FPS
     int FPS = 60;
 
+    TileManager tileM = new TileManager(this);
     Keyhandler keyh = new Keyhandler();
     Thread gameThread;
     Player player = new Player(this,keyh);
 
-    //set players default position
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4;
+  
+
 
     public GamePanel(){
 
@@ -88,7 +88,7 @@ public class GamePanel extends JPanel implements Runnable{
 
     }
     public void update(){
-
+        
         player.update();
     }
     public void paintComponent(Graphics g){
@@ -97,6 +97,8 @@ public class GamePanel extends JPanel implements Runnable{
 
         Graphics2D g2 = (Graphics2D)g;
 
+        tileM.draw(g2);
+        
         player.draw(g2);
         
         g2.dispose();
